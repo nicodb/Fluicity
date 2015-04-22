03e329cd9fe28569d1b27f380ecf39a2b34d8569
cedricmenteau <me.cedric@gmail.com>
2015-04-22 20:55:45 +0200

    add  page press

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   scope '(:locale)', locale: /fr|en/ do
     get 'thanks', to: 'home#thanks'
+    get 'press', to: 'home#press'
     resources :users, only: :create
     resources :wishes do
       member do
