29576237ffced736c28ea1dc5a7421557b0e5195
cedricmenteau <me.cedric@gmail.com>
2015-04-23 16:46:44 +0200

    change skills to issus section

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -11,4 +11,17 @@ function ShrinkNavbar(){
 
 $(document).ready(function(){
   ShrinkNavbar();
+
+  $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 2000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
+
 })
